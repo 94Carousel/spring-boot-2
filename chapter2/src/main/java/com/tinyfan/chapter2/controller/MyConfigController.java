e6141977f0112c5e77319f1e3dc8126aee2882cf
xiaofan <1271410507@qq.com>
2018-08-26 21:58:30 +0800

    init

--- chapter2/src/main/java/com/tinyfan/chapter2/controller/MyConfigController.java
+++ chapter2/src/main/java/com/tinyfan/chapter2/controller/MyConfigController.java
@@ -5,6 +5,7 @@ import com.tinyfan.chapter2.configration.Configuration2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,12 @@ public class MyConfigController {
     private final Configuration1 configuration1;
     private final Configuration2 configuration2;
 
+
+    @Value("${my.name}")
+    private String name;
+    @Value("${my.age}")
+    private int age;
+
     @Autowired
     public MyConfigController(Configuration1 configuration1, Configuration2 configuration2) {
         this.configuration1 = configuration1;
@@ -23,7 +30,7 @@ public class MyConfigController {
     }
 
     @GetMapping("/1")
-    public Configuration1 config1(){
+    public Configuration1 config1() {
         log.info("==================================");
         log.info(configuration1.toString());
         log.info("==================================");
@@ -31,10 +38,15 @@ public class MyConfigController {
     }
 
     @GetMapping("/2")
-    public Configuration2 config2(){
+    public Configuration2 config2() {
         log.info("==================================");
         log.info(configuration2.toString());
         log.info("==================================");
         return configuration2;
+    }
+
+    @GetMapping("/3")
+    public String config3() {
+        return name + age;
     }
 }
